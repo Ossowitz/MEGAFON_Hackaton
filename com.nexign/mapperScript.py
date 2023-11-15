75a919b286d509523470ba69813dca95850aa77c
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-11-15 09:48:47 +0300

    Change serializable approach for Customer entity

--- com.nexign/mapperScript.py
+++ com.nexign/mapperScript.py
@@ -7,6 +7,12 @@ class Customer:
         self.party = self.Party(data["party"])
         self.type = self.Type(data["type"])
 
+    def __dict__(self):
+        return {
+            "party": self.party.__dict__(),
+            "type": self.type.__dict__()
+        }
+
     class Party:
         def __init__(self, party_data):
             self.biometric_data = party_data["biometricData"]
@@ -23,9 +29,28 @@ class Customer:
             self.tax_registration_certificate = self.TaxRegistrationCertificate(
                 party_data["taxRegistrationCertificate"]["taxIdentificationNumber"])
 
+        def __dict__(self):
+            return {
+                "biometricData": self.biometric_data,
+                "birthDate": self.birth_date,
+                "birthPlace": self.birth_place,
+                "gender": self.gender.__dict__(),
+                "identificationDocument": self.identification_document.__dict__(),
+                "INILA": self.INILA,
+                "isResident": self.is_resident,
+                "nameInfo": self.name_info.__dict__(),
+                "nationality": self.nationality.__dict__(),
+                "publicOfficial": self.public_official,
+                "speakingLanguage": self.speaking_language.__dict__(),
+                "taxRegistrationCertificate": self.tax_registration_certificate.__dict__()
+            }
+
         class Gender:
             def __init__(self, gender_id):
                 self.gender_id = gender_id
+
+            def __dict__(self):
+                return {"genderId": self.gender_id}
 
         class IdentificationDocument:
             def __init__(self, document_data):
@@ -37,9 +62,23 @@ class Customer:
                 self.type = self.IdentificationType(document_data["type"]["identificationTypeId"])
                 self.valid_for = document_data["validFor"]
 
+            def __dict__(self):
+                return {
+                    "dateOfIssue": self.date_of_issue,
+                    "divisionCode": self.division_code,
+                    "number": self.number,
+                    "providedByOrganization": self.provided_by_organization,
+                    "series": self.series,
+                    "type": self.type.__dict__(),
+                    "validFor": self.valid_for
+                }
+
             class IdentificationType:
                 def __init__(self, identification_type_id):
                     self.identification_type_id = identification_type_id
+
+                def __dict__(self):
+                    return {"identificationTypeId": self.identification_type_id}
 
         class NameInfo:
             def __init__(self, name_info_data):
@@ -47,21 +86,40 @@ class Customer:
                 self.patronymic = name_info_data["patronymic"]
                 self.surname = name_info_data["surname"]
 
+            def __dict__(self):
+                return {
+                    "firstName": self.first_name,
+                    "patronymic": self.patronymic,
+                    "surname": self.surname
+                }
+
         class Nationality:
             def __init__(self, nationality_id):
                 self.nationality_id = nationality_id
+
+            def __dict__(self):
+                return {"nationalityId": self.nationality_id}
 
         class SpeakingLanguage:
             def __init__(self, language_id):
                 self.language_id = language_id
 
+            def __dict__(self):
+                return {"languageId": self.language_id}
+
         class TaxRegistrationCertificate:
             def __init__(self, tax_identification_number):
                 self.tax_identification_number = tax_identification_number
 
+            def __dict__(self):
+                return {"taxIdentificationNumber": self.tax_identification_number}
+
     class Type:
         def __init__(self, type_data):
             self.type_data = type_data
+
+        def __dict__(self):
+            return {"type_data": self.type_data}
 
 
 body = {
@@ -95,8 +153,8 @@ body = {
 }
 
 customer = Customer(body)
-# dumps = json.dumps(customer, default=lambda o: o.__dict__, ensure_ascii=False)
-dumps = json.dumps(customer, ensure_ascii=False)
+# dumps = json.dumps(customer, default=lambda o: o.__dict__, ensure_ascii=False, indent=4)
+dumps = json.dumps(customer.__dict__(), ensure_ascii=False, indent=4)
 print(dumps)
 
 # url = "url"
