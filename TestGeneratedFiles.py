1bd9150850b1d07258fa1c2b7bb5cfea7cfe6d1d
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-11-17 03:53:50 +0300

    Add unit-test for data creator

---
+++ TestGeneratedFiles.py
@@ -0,0 +1,30 @@
+import unittest
+import os
+import csv
+
+
+class TestGeneratedFiles(unittest.TestCase):
+
+    def test_files_creation(self):
+        people_directory = os.path.abspath("com.nexign/people")  # Укажите путь до директории с файлами
+        expected_header = ["name", "b_day", "active", "po"]
+
+        file_counts = {
+            "b2b": 7,
+            "b2c": 6
+        }
+
+        for file_type, count in file_counts.items():
+            for i in range(1, count + 1):
+                file_path = os.path.join(people_directory, f"{file_type}_{i}.csv")
+                self.assertTrue(os.path.isfile(file_path))  # Проверка наличия файла
+
+                with open(file_path, "r") as file:
+                    reader = csv.reader(file)
+
+                    # Проверка заголовка
+                    header = next(reader)
+                    self.assertEqual(header, expected_header)  # Проверка соответствия заголовка
+
+                    rows = list(reader)
+                    self.assertEqual(len(rows), 100)  # Проверка количества общих строк
